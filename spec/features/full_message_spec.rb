5a56789e998bf608166b879855aae855535366c5
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-21 11:44:33 +0000

    Add link in full message view that takes you back to the homepage

--- spec/features/full_message_spec.rb
+++ spec/features/full_message_spec.rb
@@ -7,4 +7,22 @@ feature 'Linking to full message' do
     click_link 'This message exceeds'
     expect(page).to have_content 'This message exceeds 20 characters'
   end
+
+  context 'viewing full message' do
+    background { add_and_view_full_message() }
+
+    scenario 'returning to the index page' do
+      click_link 'home'
+
+      expect(page).to have_content 'Messaging App'
+      find_field('Type message here').value
+      find_button('Submit').click
+    end
+
+    scenario 'seeing link to edit message' do
+      click_link 'edit'
+
+      expect(page).to have_content ('All your test belong to us')
+    end
+  end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -3,3 +3,10 @@ def add_message_and_submit
   fill_in :message, with: 'Just foo, no bar'
   click_button 'Submit'
 end
+
+def add_and_view_full_message
+  visit '/'
+  fill_in :message, with: 'All your test belong to us'
+  click_button 'Submit'
+  click_link 'All your test belong'
+end
